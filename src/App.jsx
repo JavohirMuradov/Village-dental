d124af2df73ed259fd9c4a05e5e9e5efe1a2266a
JavohirMuradov <javohirmuradov030@gmail.com>
2024-03-09 20:57:59 +0500

    ready

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom"
+import Layout from "./Layout/Layout"
+
+function App() {
+
+  return (
+    <Layout>
+      {/* <Routes>
+        <Route />
+      </Routes> */}
+    </Layout>
+  )
+}
+
+export default App
